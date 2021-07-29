2e3f6d2ff2b061cc23614a245a24a765aa15e210
Brandon Shockley <brandon@Brandons-MacBook-Air-ARM.local>
2021-07-28 21:21:25 -0400

    Added Notes to autoconfig.js

--- autoconfig.js
+++ autoconfig.js
@@ -1,3 +1,5 @@
 // F I R E F O X  P R E F E R N E C E S
+// Look for other tweaks like removing most icons from the 
+// urlbar like the reload icon
 pref("general.config.filename", "firefox.cfg");
 pref("general.config.obscure_value", 0);
