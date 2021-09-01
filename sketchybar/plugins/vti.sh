4758386d0cd358cb73c48f1c4d624da1c58fce36
Brandon Shockley <typkrft@icloud.com>
2021-09-01 02:59:13 -0400

    'created stack indicator for sketchybar'

--- sketchybar/plugins/vti.sh
+++ sketchybar/plugins/vti.sh
@@ -10,14 +10,13 @@ MARKET_END=1600
 AFTER_MARKET_END=2000
 CURRENT_TIME=$(date +%H%M)
 
-if [[ ${CURRENT_TIME#0} -ge ${MARKET_START} \
+if [[ ${CURRENT_TIME#0} -ge ${MARKET_START#0} \
 && ${CURRENT_TIME#0} -le ${MARKET_END} ]]; then
   VTI=$(curl -s -L https://robinhood.com/stocks/VTI \
   |  grep -E -o '[\+|\-]\d{1,4}\.\d{1,2}%' | head -n 1)
   sketchybar -m set vti label $VTI
   sketchybar -m set vti hidden off
   sketchybar -m set vti_sep hidden off
-  exit 0
 elif [[ (${CURRENT_TIME#0} -ge ${PRE_MARKET_START#0} \
 && ${CURRENT_TIME#0} -lt ${MARKET_START#0}) \
 || (${CURRENT_TIME#0} -gt ${MARKET_END#0} \
